5971fec28b1d1e08138fdca1956888a899d01d3b
mubashir-786 <mubashirrazaimran@gmail.com>
2024-05-14 15:17:52 +0500

    fix: customer list updated

--- app/(admin)/admin/customers/components/customerList.tsx
+++ app/(admin)/admin/customers/components/customerList.tsx
@@ -6,34 +6,54 @@ import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied
 import { AgGridReact } from 'ag-grid-react'; // React Data Grid Component
 import { useEffect, useState } from "react";
 
-export default function CustomerList() {
+type Prop = {
+    countries: any
+}
+export default function CustomerList({ countries }: Prop) {
     const [consignee, setConsignee] = useState<Customer[]>([]);
     const [currentPage, setCurrentPage] = useState(1);
     const itemsPerPage = 10
     const totalPages = Math.ceil(consignee.length / itemsPerPage);
-
-
-    const CustomButtonComponent = (params: any) => {
-        return <button onClick={() => window.location.href = "/admin/customers/" + params.data.customerId} className="border-3 border-[#221C63]  text-[#221C63] font-bold py-2 px-4 rounded">
-            view{" "}
-        </button>
-    };
-
-    const [colDefs, setColDefs] = useState<any>([
-        { field: "name", width: 200, filter: true, floatingFilter: true },
-        { field: "email", width: 400, filter: true, floatingFilter: true },
-        { field: "address", width: 600 },
-        { field: "Action", cellRenderer: CustomButtonComponent, flex: 1 }
-    ]);
-
     useEffect(() => {
         const getData = async () => {
             const { data } = await agent.LoadData.getAllCustomer(currentPage, itemsPerPage)
             setConsignee(data);
         };
-
         getData();
     }, []);
+    const CustomButtonComponent = (params: any) => {
+        return <button onClick={() => window.location.href = "/admin/customers/" + params.data.customerId} className="border-3 border-[#221C63]  text-[#221C63] font-bold  py-2 px-4 rounded">
+            view{" "}
+        </button>
+    };
+
+
+
+    const getCurrentCountry = (id: number) => {
+        const country = countries.find((itm: any) => itm.countryId == id)
+        // return country.countryName
+        return country
+    }
+    const CustomCountryComponent = (params: any) => {
+        console.log(countries)
+        return <p> <span className=" inline-flex items-center rounded-md px-2 py-1 text-xs font-medium  ring-1 ring-inset ">
+            <img
+                src={`/assets/images/flags/${getCurrentCountry(params.data.countryId).slug}.svg`}
+                className="img-fluid mr-3 w-7"
+                height="15px"
+                alt="Guyana flag"
+            />{" "}
+            {getCurrentCountry(params.data.countryId).countryName}
+        </span> </p>
+    };
+    const [colDefs, setColDefs] = useState<any>([
+        { field: "customerCode", width: 200, filter: true, floatingFilter: true },
+        { field: "name", width: 200, filter: true, floatingFilter: true },
+        { field: "email", width: 300, filter: true, floatingFilter: true },
+        { field: "country", width: 250, cellRenderer: CustomCountryComponent, },
+        { field: "address", width: 400 },
+        { field: "Action", cellRenderer: CustomButtonComponent, flex: 1 }
+    ]);
 
     const handleNextPage = () => {
         if (currentPage < totalPages) {
@@ -64,55 +84,17 @@ export default function CustomerList() {
                     </div>
                 </div>
                 <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                    {/* <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" className="px-6 py-3">
-                                    Name
-                                </th>
-                                <th scope="col" className="px-6 py-3">
-                                    Email
-                                </th>
-                                <th scope="col" className="px-6 py-3">
-                                    Address
-                                </th>
-                                <th scope="col" className="px-6 py-3">
-                                    Action
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {consignee.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage).map((item: Customer) => {
-                                return (
-                                    <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                                        <td className="px-6 py-4">{item.name}</td>
-                                        <td className="px-6 py-4">{item.email}</td>
-                                        <td className="px-6 py-4">{item.address}</td>
-                                        <td className="px-6 py-4 flex gap-1">
-                                            <Link href={"/admin/customers/" + item.customerId}>
-                                                <button className="border-3 border-[#221C63]  text-[#221C63] font-bold py-2 px-4 rounded">
-                                                    view{" "}
-                                                </button>
-                                            </Link>
-                                        </td>
-                                    </tr>
-                                );
-                            })}
-                        </tbody>
-                    </table> */}
+
                     <div
                         className="ag-theme-quartz" // applying the grid theme
                         style={{ height: "80vh" }} // the grid will fill the size of the parent container
                     >
-                        {/* <AgGridReact
-                            rowData={consignee.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)}
-                            columnDefs={colDefs}
-                        /> */}
+
                         <AgGridReact
                             rowData={consignee}
                             columnDefs={colDefs}
+                            rowHeight={60}
 
-                            // defaultColDef={defaultColDef}
                             rowSelection="multiple"
                             suppressRowClickSelection={true}
                             pagination={true}
--- app/(admin)/admin/customers/page.tsx
+++ app/(admin)/admin/customers/page.tsx
@@ -1,9 +1,11 @@
+import agent from "@/api/agent";
 import CustomerList from "./components/customerList";
 
-export default function page() {
+export default async function page() {
+    const countries = await agent.LoadData.countryList();
     return (
         <div className="w-full">
-            <CustomerList />
+            <CustomerList countries={countries.data} />
         </div>
     )
 }
