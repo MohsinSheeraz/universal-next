ed1a5375fe3a2bc5e1bc573aaebf706d632388ba
mubashir-786 <mubashirrazaimran@gmail.com>
2024-05-22 18:06:23 +0500

    fix: key updated

--- public/assets/scripts/extra.js
+++ public/assets/scripts/extra.js
@@ -11,10 +11,10 @@ window.CRISP_WEBSITE_ID = "18f32b59-b83b-4b08-8ca7-b0f491532437";
 
 
 window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
+function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
 
-gtag('config', 'G-54X0BRPY3M');
+gtag('config', 'G-WSSV2PN2E1');
 
 
 !function (f, b, e, v, n, t, s) {
@@ -30,4 +30,3 @@ gtag('config', 'G-54X0BRPY3M');
     'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '7334790819952347');
 fbq('track', 'PageView');
-
